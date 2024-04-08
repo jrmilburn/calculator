4ec748881e21ed0df1c3d0bb921203e7a8dd27c4
Joe Milburn <jrmilburn@outlook.com>
2024-04-08 18:44:56 +1000

    add calculator functionality

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-const button = document.querySelectorAll("button");
 const display = document.querySelector(".display");
+const calculator = document.querySelector(".calculator");
+const addBtn = document.querySelector("#add");
+const minusBtn = document.querySelector("#minus");
+const multBtn = document.querySelector("#mult");
+const divBtn = document.querySelector("#divide");
+const equal = document.querySelector("#equal");
 
 const add = function(a,b) {
-    return a+b;
+    return display.textContent+= `= ${a+b}`;
 }
 
 const subtract = function(a,b) {
-    return a-b;
+    return display.textContent+= `= ${a-b}`;
 }
 
 const multiply = function(a,b) {
-    return a*b;
+    return display.textContent+= `= ${a*b}`;
 }
 
 const divide = function(a,b) {
     if (b === 0){
         return "Error"
     }
-    return a/b;
+    return display.textContent+= `= ${a/b}`;
 }
-
-let value1;
-let value2;
-let operatorType;
 
 const operate = function(a,b,type){
     if (type === '+'){
@@ -38,4 +39,27 @@ const operate = function(a,b,type){
     }
 }
 
-console.log(button);
+const updateDisplay = function(a){
+    display.textContent += a;
+    return display.textContent;
+}
+
+const clearDisplay  = () => {
+    display.textContent='';
+    addBtn.disabled = false;
+    minusBtn.disabled = false;
+    multBtn.disabled = false;
+    divBtn.disabled = false;
+}
+
+let expression = [];
+
+equal.addEventListener("click", () => {
+    expression = updateDisplay('').split(' ');
+    let type = expression[1].charAt(0);
+    let value1 = +expression[0];
+    let value2 = +expression[1].slice(1);
+    operate(value1, value2, type);
+
+
+})
