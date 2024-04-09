306ce1d6464bee464e14f22729f909d061263a6c
Joe Milburn <jrmilburn@outlook.com>
2024-04-09 12:13:32 +1000

    Redevelop calculator logic

--- app.js
+++ app.js
@@ -1,65 +1,101 @@
-const display = document.querySelector(".display");
-const calculator = document.querySelector(".calculator");
-const addBtn = document.querySelector("#add");
-const minusBtn = document.querySelector("#minus");
-const multBtn = document.querySelector("#mult");
-const divBtn = document.querySelector("#divide");
-const equal = document.querySelector("#equal");
+let firstOperand = '';
+let secondOperand = '';
+let currentOperation = null;
+let shouldResetScreen = false;
 
-const add = function(a,b) {
-    return display.textContent+= `= ${a+b}`;
-}
+const numberBtns = document.querySelectorAll('#data-number');
+const operatorBtns = document.querySelectorAll('#data-operator');
+const equalBtn = document.querySelector('#equalsBtn');
+const clearBtn = document.querySelector('#clearBtn');
+const lastOp = document.querySelector('#lastOperationScreen');
+const currentOp = document.querySelector('.screen-current');
 
-const subtract = function(a,b) {
-    return display.textContent+= `= ${a-b}`;
-}
 
-const multiply = function(a,b) {
-    return display.textContent+= `= ${a*b}`;
-}
 
-const divide = function(a,b) {
-    if (b === 0){
-        return "Error"
+numberBtns.forEach((button) => 
+    button.addEventListener('click', () => appendNumber(button.textContent))
+);
+
+operatorBtns.forEach((button) =>
+    button.addEventListener('click', () => setOperation(button.textContent)))
+
+
+function appendNumber(number){
+    if(currentOp.textContent === '0' || shouldResetScreen){
+        resetScreen();
     }
-    return display.textContent+= `= ${a/b}`;
+    currentOp.textContent += number;
 }
 
-const operate = function(a,b,type){
-    if (type === '+'){
+const resetScreen = function(){
+    currentOp.textContent = '';
+    shouldResetScreen = false;
+}
+
+const clear = function() {
+    currentOp.textContent = '0';
+    lastOp.textContent = '';
+    firstOperand = '';
+    secondOperand = '';
+    currentOperation = '';
+}
+
+const setOperation = function(operator) {
+    if(currentOperation !== null) {evaluate}
+    firstOperand = currentOp.textContent;
+    currentOperation = operator;
+    lastOp.textContent = `${firstOperand} ${currentOperation}`;
+    shouldResetScreen = true;
+}
+
+const evaluate = function(){
+    if(currentOperation === null || shouldResetScreen) {return}
+    else if(currentOperation === '/' && currentOp.textContent === '0'){
+        currentOp.textContent = "Can't divide by 0 ya hero";
+        return
+    }
+    secondOperand = currentOp.textContent;
+    currentOp.textContent = roundResult(operate(currentOperation, firstOperand, secondOperand));
+    lastOp.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
+    currentOperation = null;
+}
+
+const roundResult = function(number){
+    return Math.round(number * 1000) / 1000;
+}
+
+const add = function(a,b){
+    return a+b;
+}
+
+const subtract = function(a,b){
+    return a-b;
+}
+
+const multiply = function(a,b){
+    return a*b;
+}
+
+const divide = function(a,b){
+    return a/b;
+}
+
+const operate = function(operator, a, b){
+    a = +a;
+    b = +b;
+    if(operator === '+'){
         return add(a,b);
-    } else if (type === '-'){
+    }else if(operator === '-'){
         return subtract(a,b);
-    } else if (type === '*'){
-        return multiply(a,b)
-    } else if (type === '/'){
-        return divide(a,b);
+    }else if(operator === '*'){
+        return multiply(a,b);
+    }else if(operator === '/'){
+        if(b===0){return null}
+        else{return divide(a,b)}
     } else {
-        return "Invalid type"
+        return "Operation Error"
     }
 }
 
-const updateDisplay = function(a){
-    display.textContent += a;
-    return display.textContent;
-}
-
-const clearDisplay  = () => {
-    display.textContent='';
-    addBtn.disabled = false;
-    minusBtn.disabled = false;
-    multBtn.disabled = false;
-    divBtn.disabled = false;
-}
-
-let expression = [];
-
-equal.addEventListener("click", () => {
-    expression = updateDisplay('').split(' ');
-    let type = expression[1].charAt(0);
-    let value1 = +expression[0];
-    let value2 = +expression[1].slice(1);
-    operate(value1, value2, type);
-
-
-})
+equalBtn.addEventListener('click', evaluate);
+clearBtn.addEventListener('click', clear);
